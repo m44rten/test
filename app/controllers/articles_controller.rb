bf4eb233d73158a5e2c28a42069d7e15852fb201
M44rten <maarten@kempenautomatisering.nl>
2016-10-17 11:18:28 +0200

    new edit show link added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
-
+	
+	def index 
+		@articles = Article.all 
+	end
+	
 	def new 
 		@article = Article.new
+	end
+	
+	def edit
+		@article = Article.find(params[:id])
 	end
 	
 	def create 
@@ -16,6 +24,17 @@ class ArticlesController < ApplicationController
 		else
 			
 			render :new
+		end
+	end
+	
+	def update 
+		@article = Article.find(params[:id])
+		
+		if @article.update(article_params) 
+			flash[:notice] = "Article was updated"
+			redirect_to edit_article_path(@article)
+		else
+			render :edit
 		end
 	end
 	
